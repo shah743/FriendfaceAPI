7d9e201b78780af9af9a9b129fd6cf2d47b0c720
Shah Zaib <shah743@users.noreply.github.com>
2023-08-19 04:15:54 +0500

    Set up routes for the API.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  resources :likes, only: [:create] do
+    collection do
+      get :most_likes
+      get :streaks
+    end
+  end
 end
