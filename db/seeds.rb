eaff38de2c1a31be0f7fe2328420c9432371ea7f
Shah Zaib <shah743@users.noreply.github.com>
2023-08-19 02:27:53 +0500

    Add CSV data import to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+file_path = Rails.root.join('public', 'friendface.csv')
+CSV.foreach(file_path, headers: true) do |row|
+  Like.create(post_id: row['post-id'], user: row['user'], date: Date.parse(row['date']))
+end
