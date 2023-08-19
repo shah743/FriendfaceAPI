4ef13fb8f745ca6913525c96edb4685163e2191f
Shah Zaib <shah743@users.noreply.github.com>
2023-08-19 08:47:04 +0500

    Add RSpec test for streaks in LikesController.

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -18,4 +18,25 @@ RSpec.describe LikesController, type: :controller do
       expect(json["likes"]).to eq(4)
     end
   end
+
+  describe "GET #streaks" do
+    context "when there's a single streak" do
+      before do
+        Like.create!(post_id: "1", user: "John", date: Date.new(2023, 8, 14)) # Monday
+        Like.create!(post_id: "2", user: "John", date: Date.new(2023, 8, 15)) # Tuesday
+        Like.create!(post_id: "3", user: "Jane", date: Date.new(2023, 8, 15)) # Tuesday
+        Like.create!(post_id: "4", user: "Jane", date: Date.new(2023, 8, 16)) # Wednesday
+        Like.create!(post_id: "5", user: "John", date: Date.new(2023, 8, 16)) # Wednesday
+        Like.create!(post_id: "6", user: "John", date: Date.new(2023, 8, 16)) # Wednesday
+      end
+
+      it "returns the correct streak data" do
+        get :streaks
+        expect(response).to be_successful
+        json = JSON.parse(response.body)
+        expect(json["streaks"].first["start"]).to eq('2023-08-14')
+        expect(json["streaks"].first["end"]).to eq('2023-08-16')
+      end
+    end
+  end
 end
